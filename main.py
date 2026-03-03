ccbf792373d9a6f1d12c86bd7530b286d994d0cc
Emma Egli <egliemma@gmail.com>
2026-03-03 10:30:37 -0500

    change center x

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ if __name__ == "__main__":
 
     BOX_W = 24
     BOX_H = 24
-    CENTER_X = 500
+    CENTER_X = 600
     CENTER_Y = 400
 
     SCHEDULE = {"hours": 5, "minutes": 0, "seconds": 0}
