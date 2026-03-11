814685b4025f5c226dab37d14263058a5d9f847a
Emma Egli <egliemma@gmail.com>
2026-03-11 09:15:40 -0400

    test

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def capture_frames(
 # ── Entry point ───────────────────────────────────────────────────────────────
 if __name__ == "__main__":
 
-    NAME = "R-C-8-1"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
+    NAME = "test"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
 
     CAMERA_INDEX = 0
     SHOW_PREVIEW = False
