3a2f46822045ccc03a5f3929febf6993c0b89409
Emma Egli <egliemma@gmail.com>
2026-03-12 17:49:02 -0400

    move center y height up

--- main.py
+++ main.py
@@ -236,7 +236,7 @@ if __name__ == "__main__":
     BOX_W = 24
     BOX_H = 24
     CENTER_X = 675
-    CENTER_Y = 350
+    CENTER_Y = 300
 
     SCHEDULE = {"hours": 8, "minutes": 0, "seconds": 0}
     test_length = timedelta(**SCHEDULE).total_seconds()
