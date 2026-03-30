6b0224471f2bfd96d63e3fb9a943f62af3966cde
Emma Egli <egliemma@gmail.com>
2026-03-30 14:23:06 -0400

    DD-C-PPTA-1

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ def capture_frames(
 if __name__ == "__main__":
 
     # NAME = "test"
-    NAME = "SS-C-0.1-9"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
+    NAME = "DD-C-PPTA-1"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
 
     CAMERA_INDEX = 0
     SHOW_PREVIEW = False
