87e8778642ec80c284088b40986ca501a69426b6
Emma Egli <egliemma@gmail.com>
2026-06-11 10:55:07 -0400

    6 min

--- main.py
+++ main.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
     CENTER_X = 675
     CENTER_Y = 300
 
-    SCHEDULE = {"hours": 5.1, "minutes": 0, "seconds": 0}
+    SCHEDULE = {"hours": 5, "minutes": 6, "seconds": 0}
     test_length = timedelta(**SCHEDULE).total_seconds()
 
     CAPTURE_INTERVAL = 6  # capture avg RGB every 6 seconds
