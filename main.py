1246f4ad37d38610baa688a1a7397b95cb974621
Emma Egli <egliemma@gmail.com>
2026-06-30 10:56:12 -0400

    F-C-1-1

--- main.py
+++ main.py
@@ -229,7 +229,7 @@ def capture_frames(
 if __name__ == "__main__":
 
     # NAME = "test"
-    NAME = "A-C-1-1"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
+    NAME = "F-C-1-1"  # the sample name -- CHANGE THIS EVERY EXPERIMENT
 
     CAMERA_INDEX = 0
     SHOW_PREVIEW = False
