546b3da252e45a4927a5e7f09de3044881d23f76
Emma Egli <egliemma@gmail.com>
2026-07-13 10:06:51 -0400

    update laser

--- main.py
+++ main.py
@@ -324,8 +324,8 @@ if __name__ == "__main__":
             "label": "pol_1",
             "box_w": 18,
             "box_h": 18,
-            "center_x": 740,  # ← set the second AOI's location
-            "center_y": 190,
+            "center_x": 735,  # ← set the second AOI's location
+            "center_y": 185,
             "color": (255, 224, 32),  # blue
         },
     ]
