ce60a4e6f401ae4bffcc9f81fb3338f61e7f119d
Emma Egli <egliemma@gmail.com>
2026-07-20 10:00:26 -0400

    change pol_1

--- main.py
+++ main.py
@@ -324,8 +324,8 @@ if __name__ == "__main__":
             "label": "pol_1",
             "box_w": 18,
             "box_h": 18,
-            "center_x": 735,  # ← set the second AOI's location
-            "center_y": 185,
+            "center_x": 750,  # ← set the second AOI's location
+            "center_y": 190,
             "color": (255, 224, 32),  # blue
         },
     ]
