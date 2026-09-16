60c96490957c2255754e6fa6fa62b837b58ebae5
Emma Egli <egliemma@gmail.com>
2026-09-16 10:03:32 -0400

    test y pos

--- main.py
+++ main.py
@@ -331,8 +331,8 @@ if __name__ == "__main__":
             "label": "pol_1",
             "box_w": 18,
             "box_h": 18,
-            "center_x": 730,  # ← set the second AOI's location
-            "center_y": 190,
+            "center_x": 750,  # ← set the second AOI's location
+            "center_y": 180,
             "color": (255, 224, 32),  # blue
         },
     ]
