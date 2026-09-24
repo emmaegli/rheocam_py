a1af30cc34c598fb6cbadb36e84ab511f98f069c
Emma Egli <egliemma@gmail.com>
2026-09-24 09:37:00 -0400

    change y pos

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ if __name__ == "__main__":
             "box_w": 18,
             "box_h": 18,
             "center_x": 740,  # ← set the second AOI's location
-            "center_y": 180,
+            "center_y": 175,
             "color": (255, 224, 32),  # blue
         },
     ]
